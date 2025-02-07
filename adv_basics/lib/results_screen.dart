0b663fd0b4a884b7a7c5b163310addedfe1d3dde
Hien <vum257792@gmail.com>
2025-02-07 16:53:30 +0700

    question_summary

--- adv_basics/lib/results_screen.dart
+++ adv_basics/lib/results_screen.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
 
+import 'package:adv_basics/data/questions.dart';
+import 'package:adv_basics/questions_summary.dart';
+
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswers});
   final List<String> chosenAnswers;
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions[i].text,
+          'correct_answer': questions[i].answers[0],
+          'user_answer': chosenAnswers[i]
+        },
+      );
+    }
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,18 +30,20 @@ class ResultsScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You answered X out of Y correctly!'),
+            const Text(
+              'You answered X out of Y correctly!',
+            ),
             const SizedBox(
               height: 30,
             ),
-            const Text('List of answers and questions...'),
+            QuestionsSummary(getSummaryData()),
             const SizedBox(
               height: 30,
             ),
             TextButton(
               onPressed: () {},
               child: const Text('Restart quiz'),
-            )
+            ),
           ],
         ),
       ),
